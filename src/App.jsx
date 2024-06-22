582bfb1ad35d0e51e66aa8346cf7ca0fa28d7bd6
ishitaraina1807 <ishitaraina18@gmail.com>
2024-06-22 16:42:36 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-center text-4xl text-red-500'>Here we go!</h1>
+    </>
+  )
+}
+
+export default App
